6edf0fe4c414f0f1e7e6066dca23d6b702ce9cc5
PraneelChugh <86180715+PraneelChugh@users.noreply.github.com>
2021-12-11 13:15:40 +0530

    Add files via upload

---
+++ kwitter_room.js
@@ -0,0 +1,30 @@
+var firebaseConfig = {
+  apiKey: "AIzaSyBQyjrjTsIQsGMGcgu-cr1HjszcHi5ZWMk",
+  authDomain: "testkwitter.firebaseapp.com",
+  databaseURL: "https://testkwitter.firebaseio.com",
+  projectId: "testkwitter",
+  storageBucket: "testkwitter.appspot.com",
+  messagingSenderId: "624653701634",
+  appId: "1:624653701634:web:2cb9a8bd873f17d92d8d1b"
+};
+
+
+
+  firebase.initializeApp(firebaseConfig);
+
+user_name = localStorage.getItem("user_name");
+
+document.getElementById("user_name").innerHTML = "Welcome " + user_name + "!";
+
+function addRoom()
+{
+  room_name = document.getElementById("room_name").value;
+
+  firebase.database().ref("/").child(room_name).update({
+    purpose : "adding room name"
+  });
+
+    localStorage.setItem("room_name", room_name);
+    
+    window.location = "kwitter_page.html";
+}
